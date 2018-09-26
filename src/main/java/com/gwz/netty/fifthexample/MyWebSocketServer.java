37e5d1fc9b4b57fa3247d6f257a2f6882ae7363f
asura7969 <1402357969@qq.com>
2018-09-26 22:16:19 +0800

    20180926新增websocket

---
+++ src/main/java/com/gwz/netty/fifthexample/MyWebSocketServer.java
@@ -0,0 +1,50 @@
+package com.gwz.netty.fifthexample;
+
+import com.gwz.netty.forthexample.MyServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class MyWebSocketServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    //handler方法针对 bossGroup 设置
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    //childHandler方法针对 workerGroup 设置
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new HttpServerCodec())
+                                    .addLast(new ChunkedWriteHandler())
+                                    .addLast(new HttpObjectAggregator(8192))
+                                    .addLast(new WebSocketServerProtocolHandler("/ws"))
+                                    .addLast(new TextWebSocketFrameHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
